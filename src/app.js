e9f60095f48eaee88703e699677afab9ae32d3bd
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-12-14 22:50:13 +0100

    updated app.js script

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// Small Physics Engine for 2D games in JavaScript 
+
+
+// Global variables
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+var gravity = 0.5;
+var friction = 0.9;
+
+
+
