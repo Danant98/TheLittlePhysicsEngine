1ceb59accbc86e62f41e1cfc29e33e61385b9eb0
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-12-15 15:02:47 +0100

    updated files

--- src/app.js
+++ src/app.js
@@ -6,8 +6,7 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
-var gravity = 0.5;
-var friction = 0.9;
+
 
 
 
